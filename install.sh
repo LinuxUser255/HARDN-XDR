8f99dffbb99cd49be76be6679f8026f79d75bf99
LinuxUser255 <atnl@protonmailcom>
2025-06-28 16:06:51 -0400

    removed scrap text file.

--- install.sh
+++ install.sh
@@ -11,16 +11,23 @@ update_system() {
         apt update && apt upgrade -y
 }
 
+
+check_git() {
+        printf "\033[1;31m[+] Checking if git is installed, and installing it if not..\033[0m\n"
+        [ -x "$(command -v git)" ] && printf "\033[1;32m[+] Git is installed.\033[0m\n" || { sudo apt install git -y; printf "\033[1;32m[+] Git is now installed.\033[0m\n"; }
+}
+
 # Git clone the repo, then cd into the repo and run the script hardn-main.sh
-run() {
+retrieve_repo() {
         git clone https://github.com/OpenSource-For-Freedom/HARDN-XDR.git
-        cd HARDN-XDR/src/setup && chmod +x hardn-main.sh && sudo ./hardn-main.sh
+        cd HARDN-XDR/src/setup &&  chmod +x hardn-main.sh && sudo ./hardn-main.sh
 }
 
 main() {
         check_root
         update_system
-        run
+        check_git
+        retrieve_repo
 }
 
 main
